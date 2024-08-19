7bad7ee30ab634f66030978b0270509058279f8a
fangaranda17 <106360530+fangaranda17@users.noreply.github.com>
2024-08-19 16:45:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = joblib.load('random_forest_iris_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-   try:
+   
         data = request.get_json()
         sepal_length = data['sepal_length']
         sepal_width = data['sepal_width']
@@ -19,10 +19,7 @@ def predict():
         prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
 
         return jsonify({'prediction': int(prediction[0])})
-    except Exception as e:
-        # Log the error and return a custom error message
-        print(f"Error: {e}")
-        return jsonify({"error": "Internal server error"}), 500
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
